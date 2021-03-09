d98cf842e816fade57dedc2974b5602dc4319beb
Rafael Silva <rasilva.1986@gmail.com>
2021-03-09 02:52:01 -0800

    Created the project

---
+++ src/ComputerOperations.java
@@ -0,0 +1,10 @@
+
+public interface ComputerOperations {
+	public void turnOn();	
+	public void turnOff();
+	public void performDownload();
+	public void performUpload();
+	public void playVideo();
+	public void playMusic();	
+
+}
